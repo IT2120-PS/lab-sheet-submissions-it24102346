97b66e8beffbac2c55bfd7596e7ed3677c2cae2f
it24102346 <it24102346@my.sliit.lk>
2025-09-16 11:33:16 +0530

    Add files via upload

---
+++ Lab Sheet 07/LAB7-EXE2.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\sades\\OneDrive\\Desktop\\PS-LAB7")
+
+\\(1)
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+\\(2)
+pexp(2,rate=0.33,lower.tail=TRUE)
+
+\\(3-i)
+1-pnorm(130, mean = 100, sd = 15, lower.tail = TRUE)
+
+\\(3-ii)
+qnorm(0.95,mean=100,sd=15,lower.tail=TRUE)
